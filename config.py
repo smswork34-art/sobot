f77ef8f7d2f3062caab06928f4a2c1f07cf520d5
smswork34-art <smswork34@gmail.com>
2026-05-12 00:31:04 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Токен бота из @BotFather
+BOT_TOKEN = "8490748623:AAGSbOoSItyMng1jtxn4C-8xoE0J3iXpuWg"
+
+# URL вашего приложения на Render
+WEBAPP_URL = "https://your-app.onrender.com"
+
+# Настройки крипто платежей
+CRYPTO_API_KEY = "575343:AA8lI3rebCZuc9HxysqN073qP3jLgrz2sx8"
+CRYPTO_API_URL = "https://api.cryptoprocessing.io/v2"
